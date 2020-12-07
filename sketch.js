df76cfe3bb028a845073026d7b6dd7bf58a0fb0a
cuckoo11 <73861547+cuckoo11@users.noreply.github.com>
2020-12-08 02:14:52 +0900

    Life 기본 생성

--- sketch.js
+++ sketch.js
@@ -1,7 +1,22 @@
+let life;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(300, 1000);
+  life = new Life();
 }
 
 function draw() {
   background(220);
+  life.show();
+}
+
+class Life {
+  constructor() {
+    this.pos = createVector(mouseX,mouseY);
+  }
+
+  show(){
+    ellipse(mouseX-1,mouseY - 1,50,50);
+    fill(255,200);
+  }
 }
