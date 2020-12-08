4c0079569351398cd728bf77d10a26b787135d5b
cuckoo11 <73861547+cuckoo11@users.noreply.github.com>
2020-12-08 10:13:37 +0900

    Life Move

--- sketch.js
+++ sketch.js
@@ -8,15 +8,31 @@ function setup() {
 function draw() {
   background(220);
   life.show();
+  life.move();
+  life.update();
 }
 
 class Life {
   constructor() {
-    this.pos = createVector(mouseX,mouseY);
+    this.pos = createVector(10,10);
+    this.vel = createVector(0,0);
+    this.acc = createVector(0,0);
   }
 
+  update(){
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+
+    this.pos.x = this.pos.x + random(-2, 2);
+    this.pos.y = this.pos.y + random(-2, 2);
+  }
   show(){
-    ellipse(mouseX-1,mouseY - 1,50,50);
-    fill(255,200);
+    ellipse(this.pos.x,this.pos.y,50,50);
+    fill(255,255,255,90);
+  }
+
+  move(){
+    this.vel.x = (mouseX-this.pos.x)/10;
+    this.vel.y = (mouseY-this.pos.y)/10;
   }
 }
